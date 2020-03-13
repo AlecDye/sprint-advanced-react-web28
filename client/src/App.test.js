7d0374ae3a738efc459cb09d27b28d54cb61e2e6
Alec Dye <alecdye@gmail.com>
2020-03-13 09:16:43 -0700

    Building integration app tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Components
+import App from "./App";
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+// Testing if entire App renders
+test("Rendering App without crashing", () => {
+  render(<App />);
 });
