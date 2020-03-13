de003b9c45c35b85a4df3a53e3d9862dc9019dd4
Alec Dye <alecdye@gmail.com>
2020-03-13 08:20:03 -0700

    Building app structure

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// Stateless function; pass in props to render
+function PlayerCard() {
+  return <div className="player-card"></div>;
+}
+
+export default PlayerCard;
---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Stateful class component; axios call and map to PlayerCard
+class PlayerList extends React.Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default PlayerList;
