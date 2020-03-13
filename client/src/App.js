ef3c76aa65e5c28c2e948fe8329a67f4cb24f312
Alec Dye <alecdye@gmail.com>
2020-03-13 08:47:49 -0700

    Building local storage hook

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 
 // Component imports
+import Header from "./components/Header";
 import PlayerList from "./components/PlayerList";
 
+// Stateless function component (wrapper)
 function App() {
   return (
     <div className="App">
+      <Header />
       <PlayerList />
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Women's WorldCup June 2019 - July 2019</h1>
+    </header>
+  );
+}
+
+export default Header;
