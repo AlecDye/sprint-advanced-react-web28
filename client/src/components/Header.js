322f440b7f9a9a17d61d3cab6679d53a71f22f65
Alec Dye <alecdye@gmail.com>
2020-03-13 09:09:52 -0700

    Building darkmode toggle in CSS

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+// Components
+import { useDarkMode } from "../hooks/useDarkMode";
+
 function Header() {
+  const [theme, setTheme] = useDarkMode("dark-mode", false);
+  function toggleTheme(event) {
+    event.preventDefault();
+    // on toggle: set theme to opposite of current value (true/false)
+    setTheme(!theme);
+  }
   return (
     <header>
       <h1>Women's WorldCup June 2019 - July 2019</h1>
+      <div className="dark-mode___toggle">
+        <div
+          onClick={toggleTheme}
+          className={theme ? "toggle toggled" : "toggle"}
+        />
+      </div>
     </header>
   );
 }
